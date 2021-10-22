169395e95de8534f6445b53282986b047deb2457
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-22 18:25:18 +0530

    Added password of 8 characters

--- src/UserRegisteration.java
+++ src/UserRegisteration.java
@@ -46,4 +46,15 @@ public class UserRegisteration {
 			System.out.println("invalid phoneNumber");
 			}
         }
+
+	public void password(String password) {
+		Pattern pattern = Pattern.compile("^[a-zA-z]{8}$");
+		Matcher match = pattern.matcher(password);
+		if(match.find() && match.group().equals(password)) {
+			System.out.println("valid password");	
+			}
+		else {
+			System.out.println("invalid password");
+			}
+        }
 }
