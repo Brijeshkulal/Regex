4fdc6d8df0d6e0bad7fd1f8927161ce9439965fb
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-22 13:30:56 +0530

    Added pattern for firstname

--- src/UserRegisteration.java
+++ src/UserRegisteration.java
@@ -5,7 +5,16 @@ import java.util.regex.Pattern;
 public class UserRegisteration {
 	
 	public static void main(String[] args) {
-		System.out.println("welcome");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first name: ");
+		String firstName = sc.next();	
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		Matcher match = pattern.matcher(firstName);
+		if(match.find() && match.group().equals(firstName)) {
+			System.out.println("valid FirstName");	
 			}
-        
+		else {
+			System.out.println("invalid FirstName");
+			}
+        }
 }
