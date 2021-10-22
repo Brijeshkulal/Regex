6dbc8182f2798a14b6dbbd01cd8b46fcc1e38a52
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-22 13:29:21 +0530

    Added welcome message

---
+++ src/UserRegisteration.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegisteration {
+	
+	public static void main(String[] args) {
+		System.out.println("welcome");
+			}
+        
+}
