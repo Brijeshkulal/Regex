85065b71cd4c8fe31d81ac1223fa1d4a9603d430
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-25 12:58:55 +0530

    Added sample Emails id and checked the output

--- src/RegisterationMain.java
+++ src/RegisterationMain.java
@@ -2,29 +2,51 @@ import java.util.Scanner;
 
 public class RegisterationMain {
 	
-	public static void main(String[] args) {
-		UserRegisteration ur = new UserRegisteration();
-		Scanner sc = new Scanner(System.in);
+	UserDetails userDetails = new UserDetails();
+	Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter the first name: ");
-		String fName = sc.next();
-		ur.firstName(fName);
-		
-		System.out.println("Enter the Last name: ");
-		String lName = sc.next();
-		ur.lastName(lName);
-		
-		System.out.println("Enter the Email: ");
-		String mailId = sc.next();
-		ur.emailId(mailId);
-		
-		System.out.println("Enter the Mobile Number: ");
-		String mobileNumber = sc.next();
-		ur.phoneNumber(mobileNumber);
-		
-		System.out.println("Enter the Password: ");
-		String password = sc.next();
-		ur.password(password);
-	}
-
+		public void choice() {
+	        while (true) {
+	            System.out.println("Enter 1 To Check First Name\n"
+	                             + "Enter 2 To Check Second Name\n"
+	                             + "Enter 3 To Check EmailId\n"
+	                             + "Enter 4 To Check Mobile Number\n"
+	                             + "Enter 5 To Check Password\n"
+	                             + "\nEnter 6 To test the sample EmailIds\n"
+	                             + "Enter 0 To Exit");
+	            int choice = sc.nextInt();
+	            switch (choice) {
+	                case 1 :
+	                	userDetails.userFirstName();
+	                    break;
+	                case 2 :
+	                	userDetails.userLastName();
+	                    break;
+	                case 3 :
+	                	userDetails.userEmail();
+	                    break;
+	                case 4 :
+	                	userDetails.userMobile();
+	                    break;
+	                case 5 :
+	                	userDetails.userPassword();
+	                    break;
+	               case 6 :
+	            	   userDetails.sampleEmail();
+	            	   break;
+	                case 0:
+	                    System.exit(0);
+	                    break;
+	                default :
+	                    System.out.println("Enter the wrong input \n Please enter the Correct input");
+	                    continue;
+	            }
+	        }
+		}
+	
+		public static void main(String[] args) {
+			RegisterationMain main = new RegisterationMain();	
+			main.choice();
+		}
 }
+	          
